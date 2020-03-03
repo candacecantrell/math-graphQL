c44232b32e92c201bb4d65665f6ec5e49db276f2
Maurina_Davis <maurina.davis@gmail.com>
2020-03-03 12:46:54 -0700

    general error handling created

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -7,6 +7,19 @@ const User = require('../models/user-model')
 
 module.exports = {
         createUser: async function ({userInput}, req){
+            const errors = []
+            if(!validator.isEmail(userInput.email)){
+                errors.push({message: 'Email is invalid'})
+            }
+            if (validator.isEmpty(userInput.password) || !validator.isLength(userInput.password, { min: 8})){
+                errors.push({message: 'Password is not long enough'})
+            }
+            if (errors.length > 0) {
+                const error = new Error('Invalid input')
+                error.data = errors
+                error.code = 422
+                throw error
+            }
             const existingUser = await User.findOne({email: userInput.email})
             if (existingUser){
                 const error = new Error('User already exists')
